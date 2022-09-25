223482247eb65e7fdef23feeeb1b8835dcd433cb
3OW <mail@lehmannfelix.de>
2022-09-25 09:43:28 +0200

    add startServerDev.sh

---
+++ startServerDev.sh
@@ -0,0 +1,3 @@
+npm run start:dev --prefix gessages-server/gessages-server-users-api/ &
+npm run start:dev --prefix gessages-server/gessages-server-messages-api/ &
+npm run start:dev --prefix gessages-server/gessages-server-api-gateway/ && fg
