04b18adbc0c943291bd59cb0e8cebcf182ca756c
3OW <mail@lehmannfelix.de>
2022-09-26 21:56:09 +0200

    add server install script

---
+++ installServer.sh
@@ -0,0 +1,3 @@
+cd gessages-server/gessages-server-users-api/ && npm install && cd ..
+cd gessages-server-messages-api/ && npm install && cd ..
+cd gessages-server-api-gateway && npm install && cd ../../
